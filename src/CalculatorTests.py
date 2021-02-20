220bb4b9b20e1056ef071dfe562aa87770b3e8cd
Franklin Tan <fjt7@njit.edu>
2021-02-20 02:46:53 -0500

    Feature: Added Addition method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -4,9 +4,14 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
+    
+    def test_results_addition_method(self):
+        self.assertEqual(self.calculator.add(5, 5), 10)
 
 
 if __name__ == '__main__':
