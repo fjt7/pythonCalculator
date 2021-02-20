8dabc867b256c98b84fae020f176c52d00947ed9
Franklin Tan <fjt7@njit.edu>
2021-02-20 02:52:41 -0500

    Feature: Added Subtraction method

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,6 +3,13 @@ def addition(a, b):
     return c
 
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
 class Calculator:
     result = 0
 
@@ -11,4 +18,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(b, a)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -9,9 +9,12 @@ class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
-    
+
     def test_results_addition_method(self):
         self.assertEqual(self.calculator.add(5, 5), 10)
+
+    def test_results_subtraction_method(self):
+        self.assertEqual(self.calculator.subtract(12, 4), 8)
 
 
 if __name__ == '__main__':
